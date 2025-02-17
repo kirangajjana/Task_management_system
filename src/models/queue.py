a1c60f6c87aa5f8ed6e1cfdcd1f7d5caa30166b6
kiran gajjana <kiran.gajjana@gmail.com>
2025-02-17 15:52:49 +0530

    queue code update

---
+++ src/models/queue.py
@@ -0,0 +1,17 @@
+class Queue:
+    def __init__(self):
+        self.item=[]
+        print("you re in queue class")
+     
+     
+    def Enqueue(self,data):
+            self.item.append(data)
+    
+    def Dequeue(self):
+        self.item.remove(0)        
+        
+    def is_empty(self):
+        if len(self.item)==0:
+            return True 
+        else:
+            return False   
