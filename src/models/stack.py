ed362c88a401a1f7c08573bd6229574d8e6ab86d
kiran gajjana <kiran.gajjana@gmail.com>
2025-02-17 15:58:08 +0530

    stack update

---
+++ src/models/stack.py
@@ -0,0 +1,13 @@
+class Stack:
+    def __init__(self,item): #initialsising empty list
+        self.item=[]
+    def push(self,data):  #pushing elements into the stack
+        self.item.append(data)    
+    def pop(self):    #poping elements from the stack
+        self.item.pop()
+    def is_empty(self):  #checking the elements inside the stack
+        if len(self.item)==0:
+            return True
+        else:
+            return False
+                
